bcf3d9c603c2021e259f44fc9606499dab59211c
fancidev <fancidev@gmail.com>
2023-01-07 20:27:59 +0800

    Refactor automorphism code.

--- mastermind/canonical.hpp
+++ mastermind/canonical.hpp
@@ -5,6 +5,7 @@
 #include <bit>
 #include <bitset>
 #include <cassert>
+#include <concepts>
 
 #include "thirdparty/fixed_capacity_vector"
 
@@ -15,46 +16,47 @@
 
 namespace mastermind {
 
-/// Represents a permutation of (position) indices {0, ..., m-1}.
-class PositionPermutation
+/// Represents a permutation of the index set {0, ..., n-1} of type T,
+/// supporting up to Capacity indices.
+template <std::integral T, size_t Capacity>
+class Permutation
 {
 public:
-    /// Creates an identity position permutation.
-    constexpr explicit PositionPermutation(const CodewordRules &rules) noexcept
-      : _inverse(rules.codeword_length())
+    /// Type of the index.
+    using Index = T;
+
+    /// Creates an identity permutation of the given size.
+    constexpr explicit Permutation(size_t size) noexcept : _map(size)
     {
-        std::iota(_inverse.begin(), _inverse.end(), PositionIndex(0));
+        std::iota(_map.begin(), _map.end(), Index(0));
     }
 
-    /// Changes to the next position permutation in lexical order.
+    /// Updates the permutation to the next one in lexicographical order.
     ///
-    /// Returns `false` if the new permutation is the identity permutation;
-    /// otherwise returns `true`.
+    /// Returns `false` if the updated permutation is the identity
+    /// permutation, or `true` otherwise.
     constexpr bool next() noexcept
     {
-        return std::next_permutation(_inverse.begin(), _inverse.end());
+        return std::next_permutation(_map.begin(), _map.end());
     }
 
-    constexpr PositionSize size() const noexcept
-    {
-        return _inverse.size();
-    }
+    /// Returns the number of elements in the index set being permuted.
+    constexpr size_t size() const noexcept { return _map.size(); }
 
-    /// Returns the inverse of j' under this permutation.  That is,
-    /// if j == inv(j'), then j' == map(j).
-    constexpr PositionIndex inv(PositionIndex j_prime) const noexcept
+    /// Returns the image of `index` under this permutation.
+    constexpr Index map(const Index &index) const noexcept
     {
-        assert(j_prime >= 0 && j_prime < _inverse.size());
-        return _inverse[j_prime];
+        assert(index >= 0 && index < _map.size());
+        return _map[index];
     }
 
     /// Returns `true` if this is the identity permutation.
     constexpr bool is_identity() const noexcept
     {
-        const PositionSize m = _inverse.size();
-        for (PositionIndex j = 0; j < m; j++)
+        const size_t m = _map.size();
+        for (Index j = 0; j < m; j++)
         {
-            if (_inverse[j] != j)
+            if (_map[j] != j)
                 return false;
         }
         return true;
@@ -62,210 +64,107 @@ public:
 
 private:
     /// Stores the inverse permutation, because it is what we will use.
-    std::experimental::fixed_capacity_vector<PositionIndex, MAX_CODEWORD_LENGTH>
-        _inverse;
+    std::experimental::fixed_capacity_vector<Index, Capacity> _map;
 };
 
-/// Represents a bijection from a subset of k (alphabet) indices in the range
-/// {0, ..., n-1} to the first k (alphabet) indices (0, ..., k-1).
-class AlphabetPermutation // rename to AlphabetBijection, or just bijection
+/// Represents a bijection from a k-subset of the index set {0, ..., n-1}
+/// to the index set {0, ..., k-1}.  In addition, the bijection is updated
+/// automatically when an unmapped index is queried -- it is automatically
+/// mapped to the least image.
+template <std::integral Index, size_t Capacity>
+class PartialPermutation
 {
 public:
-    static constexpr AlphabetIndex NotMapped = AlphabetIndex(-1);
+    static constexpr Index not_mapped = Index(-1);
 
     /// Creates an empty bijection.
-    constexpr explicit AlphabetPermutation(const CodewordRules &rules) noexcept
-      : _map(rules.alphabet_size(), NotMapped), _num_mapped(0) { }
-
-//    constexpr bool is_mapped(AlphabetIndex i) const noexcept
-//    {
-//        assert(i >= 0 && i < _mapping.size());
-//        return (_mapped & (size_t(1) << i)) != size_t(0);
-//    }
-
-    /// Returns `true` if the entire alphabet index set {0, ..., n-1}
-    /// is mapped.
-    constexpr bool is_permutation() const noexcept
+    constexpr explicit PartialPermutation(size_t n) noexcept
+      : _map(n, not_mapped), _next_image(0)
     {
-        return _num_mapped == _map.size();
+        assert(n >= 0 && n <= Capacity);
     }
 
-    /// Returns `true` if the bijection is the identity permutation.
+    /// Returns `true` if every index in the range {0, ..., n-1} is
+    /// explicitly mapped.
+    constexpr bool is_full() const noexcept
+    {
+        return _next_image == _map.size();
+    }
+
+    /// Returns `true` if every index in the range {0, ..., n-1} is
+    /// explicitly mapped to itself.
     constexpr bool is_identity() const noexcept
     {
-        const AlphabetSize n = _map.size();
-        for (AlphabetIndex i = 0; i < n; i++)
+        Index index = Index(0);
+        for (Index image : _map)
         {
-            if (_map[i] != i)
+            if (image != index++)
                 return false;
         }
         return true;
     }
 
-    /// Returns the number of letters mapped by this bijection.
-    constexpr AlphabetSize size() const noexcept
-    {
-        return _num_mapped;
-    }
-
-    constexpr AlphabetIndex map(AlphabetIndex i) const noexcept
-    {
-        assert(i >= 0 && i < _map.size());
-        return _map[i];
-    }
-
-    /// Updates the bijection by mapping `index` to `image`.
+    /// Returns the image of `index` under this bijection.
     ///
-    /// `index` must not be mapped, and `image` must be the smallest
-    /// available image.
-    constexpr void map(AlphabetIndex index, AlphabetIndex image) noexcept
+    /// If `index` is not explicitly mapped, it is automatically mapped
+    /// to the least unmapped image and the bijection object is updated.
+    /// That is why this member function is not `const`.
+    constexpr Index map(Index index) noexcept
     {
         assert(index >= 0 && index < _map.size());
-        assert(_map[index] == NotMapped);
-        assert(image == _num_mapped);
-
-        _map[index] = image;
-        ++_num_mapped;
+        if (_map[index] == not_mapped)
+            _map[index] = _next_image++;
+        return _map[index];
     }
 
 private:
-    /// `_map[i]` is the image under the bijection, or the constant
-    /// NotMapped if `i` is not mapped.
-    std::experimental::fixed_capacity_vector<AlphabetIndex, MAX_ALPHABET_SIZE>
-        _map;
+    /// `_map[index]` is the image under the bijection, or `not_mapped`
+    /// if `index` is not explicitly mapped.
+    std::experimental::fixed_capacity_vector<Index, Capacity> _map;
 
-    /// `_mapped[i]` is `true` if and only if `_mapping[i]` is valid.
-    AlphabetSize _num_mapped;
+    /// The next image to map an unmapped index to.
+    Index _next_image;
 };
 
-///// Represents a set of permutations of the codeword, where each permutation
-///// is the composition of:
-///// - a fixed permutation of (all) the positions;
-///// - a fixed permutation of a subset of the alphabet; and
-///// - any one of the permutations of the other subset of the alphabet.
-//class CodewordPermutation
-//{
-//	/// `j' = _position_map[j]` maps position j to j', `0 <= j, j' < m`.
-//    PositionMap _position_map;
-//
-//    /// `j = _inverse_position_map[j']` maps position j' back to j.
-//    PositionMap _inverse_position_map;
-//
-//    /// `i' = _letter_map[i]` maps letter i to i` for `0 <= i < n` where
-//    /// `_letter_mapped[i]` is true.  If `_letter_mapped[i]` is false,
-//    /// `_letter_map[i]` is unspecified and should not be used.
-//    LetterMap _letter_map;
-//
-//    /// `_letter_mapped[i]` is `true` if the i-th letter is mapped.
-//    std::bitset<MAX_ALPHABET_SIZE> _letter_mapped;
-//
-//public:
-//
-//	/// Creates an identity permutation.
-//	constexpr CodewordPermutation(const CodewordRules &rules) noexcept
-//      : _position_map(rules.codeword_length()),
-//        _inverse_position_map(rules.codeword_length()),
-//        _letter_map(rules.alphabet_size()),
-//        _letter_mapped()
-//	{
-//        std::iota(_letter_map.begin(), _letter_map.end(), AlphabetIndex(0));
-//        std::iota(_position_map.begin(), _position_map.end(), PositionIndex(0));
-//        _inverse_position_map = _position_map;
-//        _letter_mapped.set();
-//	}
-//
-//    /// Returns the mapped value of position j.
-//    constexpr PositionIndex map_position(PositionIndex j) const noexcept
-//    {
-//        assert(j >= 0 && j < _position_map.size());
-//        return _position_map[j];
-//    }
-//
-//    /// Returns the position that is mapped to j.
-//    constexpr PositionIndex inverse_map_position(PositionIndex j) const noexcept
-//    {
-//        assert(j >= 0 && j < _inverse_position_map.size());
-//        return _inverse_position_map[j];
-//    }
-//
-//    AlphabetIndex map_letter(AlphabetIndex i)
-//    {
-//        if (_letter_mapped[i])
-//        {
-//            return _letter_map[i];
-//        }
-//        else
-//        {
-//            int least = std::countr_zero(_letter_mapped);
-//            return _letter_map[i] = static_cast<AlphabetIndex>(least);
-//        }
-//    }
-//
-//#if 0
-//	/// Returns the inverse of the permutation.
-//	CodewordPermutation inverse() const
-//	{
-//		CodewordPermutation ret(_rules);
-//		for (int i = 0; i < _rules.pegs(); ++i)
-//		{
-//			if (peg[i] >= 0)
-//				ret.peg[peg[i]] = i;
-//		}
-//		for (int i = 0; i < _rules.colors(); ++i)
-//		{
-//			if (color[i] >= 0)
-//				ret.color[color[i]] = i;
-//		}
-//		return ret;
-//	}
-//#endif
+/// Bundles an inverse position permutation with a partial alphabet
+/// permutation.
+struct CodewordPermutation
+{
+    /// Represents a permutation of (position) indices {0, ..., m-1}.
+    using PositionPermutation = Permutation<PositionIndex, MAX_CODEWORD_LENGTH>;
 
-//	/// Permutes the pegs and colors in a codeword.
-//	Codeword permute(const Codeword &w) const
-//	{
-//		Codeword ret;
-//		for (int i = 0; i < MM_MAX_PEGS && w[i] != Codeword::EmptyColor; ++i)
-//		{
-//			ret.set(peg[i], color[w[i]]);
-//		}
-//		return ret;
-//	}
-//
-//	/// Permutes the pegs in a codeword.
-//	Codeword permute_pegs(const Codeword &w) const
-//	{
-//		Codeword ret;
-//		for (int i = 0; i < MM_MAX_PEGS /* && w[i] != 0xFF */; ++i)
-//			ret.set(peg[i], w[i]);
-//		return ret;
-//	}
-//
-//#if 0
-//	Codeword permute_colors(const Codeword &w) const
-//	{
-//		Codeword ret;
-//		for (int i = 0; i < _rules.pegs(); ++i)
-//			ret.set(i, color[w[i]]);
-//		return ret;
-//	}
-//#endif
-//};
+    /// Represents a bijection from a subset of k alphabet indices
+    /// in the range {0, ..., n-1} to the first k alphabet indices
+    /// {0, ..., k-1}.  The bijection grows automatically when an
+    /// unmapped index is mapped.
+    using AlphabetPermutation =
+        PartialPermutation<AlphabetIndex, MAX_ALPHABET_SIZE>;
+
+    PositionPermutation inverse_position_permutation;
+    AlphabetPermutation partial_alphabet_permutation;
+
+    explicit constexpr CodewordPermutation(const CodewordRules &rules) noexcept
+      : inverse_position_permutation(rules.codeword_length()),
+        partial_alphabet_permutation(rules.alphabet_size())
+    {
+    }
+};
 
 /// Represents the set of all automorphisms (permutation of letters and
 /// positions) that map a guess sequence to itself.
+/// TODO: include the letter sequence
 class AutomorphismGroup
 {
 public:
     /// Creates the automorphism group for an empty guess sequence.
     explicit AutomorphismGroup(const CodewordRules &rules)
     {
-        PositionPermutation position_perm(rules);
-        AlphabetPermutation alphabet_perm(rules); // fully unrestricted
+        CodewordPermutation perm(rules);
         do
         {
-            _perms.push_back(std::make_pair(position_perm, alphabet_perm));
+            _perms.push_back(perm);
         }
-        while (position_perm.next());
+        while (perm.inverse_position_permutation.next());
     }
 
     /// Returns `true` if this automorphism group contains exactly one
@@ -275,10 +174,11 @@ public:
         assert(!_perms.empty());
         if (_perms.size() == 1)
         {
-            assert(_perms[0].first.is_identity());
-            if (_perms[0].second.is_permutation())
+            const CodewordPermutation &perm = _perms.front();
+            assert(perm.inverse_position_permutation.is_identity());
+            if (perm.partial_alphabet_permutation.is_full())
             {
-                assert(_perms[0].second.is_identity());
+                assert(perm.partial_alphabet_permutation.is_identity());
                 return true;
             }
         }
@@ -297,29 +197,22 @@ public:
         // in the group, the mapped `guess` is greater than or equal to
         // `guess` in lexical order.
 
-        std::vector<std::pair<PositionPermutation, AlphabetPermutation>> perms;
+        std::vector<CodewordPermutation> perms;
 
-        for (const std::pair<PositionPermutation, AlphabetPermutation> &perm : _perms)
+        for (CodewordPermutation perm : _perms)
         {
-            PositionPermutation position_perm = perm.first;
-            AlphabetPermutation alphabet_perm = perm.second;
-
             // To check whether h := perm(g) < g in lexical order,
             // we compare h[j] vs g[j] for each j = 0, ..., m-1.
             // Note that h[j] = letter_map[g[inverse_position_map[j]].
 
             bool is_automorphism = true;
-            const PositionSize m = position_perm.size();
+            const PositionSize m = perm.inverse_position_permutation.size();
             for (PositionIndex j = 0; j < m && is_automorphism; j++)
             {
                 AlphabetIndex index = letters[j];
-                PositionIndex pos = position_perm.inv(j);
-                AlphabetIndex image = alphabet_perm.map(letters[pos]);
-                if (image == AlphabetPermutation::NotMapped)
-                {
-                    image = alphabet_perm.size();
-                    alphabet_perm.map(letters[pos], image);
-                }
+                PositionIndex pos = perm.inverse_position_permutation.map(j);
+                AlphabetIndex image = perm.partial_alphabet_permutation.map(letters[pos]);
+
                 if (image < index) // guess is not canonical
                     return false;
                 if (image > index) // not an automorphism
@@ -327,7 +220,7 @@ public:
             }
 
             if (is_automorphism)
-                perms.push_back(std::make_pair(position_perm, alphabet_perm));
+                perms.push_back(perm);
         }
 
         std::swap(perms, _perms);
@@ -335,7 +228,8 @@ public:
     }
 
 private:
-    std::vector<std::pair<PositionPermutation, AlphabetPermutation>> _perms;
+    /// List of all the permutations that map a guess sequence to itself.
+    std::vector<CodewordPermutation> _perms;
 };
 
 ///// Outputs a codeword permutation to a stream.
